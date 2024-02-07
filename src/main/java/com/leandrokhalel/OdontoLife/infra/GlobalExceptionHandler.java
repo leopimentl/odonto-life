bcfd260155e5770b6f3e4c39d5ef726f96a81bca
Leandro Khalel <leandrokhalelpimentel@gmail.com>
2024-02-07 00:49:17 -0300

    Tratamento de erros na API

--- src/main/java/com/leandrokhalel/OdontoLife/infra/GlobalExceptionHandler.java
+++ src/main/java/com/leandrokhalel/OdontoLife/infra/GlobalExceptionHandler.java
@@ -3,11 +3,21 @@ package com.leandrokhalel.OdontoLife.infra;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private record DadosErroValidacao(String campo, String mensagem) {
+        public DadosErroValidacao(FieldError fieldError){
+            this(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+    }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<Void> handleEntityNotFound() {
@@ -16,4 +26,14 @@ public class GlobalExceptionHandler {
                 .status(HttpStatus.NOT_FOUND)
                 .build();
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<DadosErroValidacao>> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+
+        var erros = ex.getFieldErrors();
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
 }
